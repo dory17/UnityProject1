c5a4598326fc044cd1454bd467d68d0aed593ddb
Laurie <lauriejenk007@gmail.com>
2017-03-01 16:11:03 -0700

    Lists

---
+++ New Unity Project/Assets/Lists.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class Lists : MonoBehaviour {
+	public string animal;
+	public List<string> zooRoster = new List<string>();
+
+
+
+	// Use this for initialization
+	void Start () {
+		zooRoster.Add ("monkey");
+		zooRoster.Add ("elephant");
+		zooRoster.Add ("alligator");
+		zooRoster.Add ("lion");
+	
+		}
+	public void AddAnimal()
+	{
+		if (animal != "") {
+			{
+				if (zooRoster.Contains (animal) != true) {
+					zooRoster.Add (animal);
+				}
+			}
+		}
+	}
+	public void DeleteAnimal ()
+
+	{
+		if (zooRoster.Contains (animal))
+			zooRoster.Remove (animal);
+	}
+
+	public void ClearList ()
+	{
+		if (zooRoster.Contains (animal))
+			zooRoster.Clear ();
+		
+		}
+
+	public void PrintAllAnimals()
+		{
+			
+		System.Console.WriteLine(animal);
+		}
+		
+		
+}
